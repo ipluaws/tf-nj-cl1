f6d80dab496eb5a7de66ba72aa9205e914497410
ipluaws <67364414+ipluaws@users.noreply.github.com>
2024-08-02 11:25:43 +0200

    Create variables.tf

---
+++ deploy/variables.tf
@@ -0,0 +1,5 @@
+variable "container_image" {
+  description = "The container image for the ECS task"
+  type        = string
+  default     = "654654148473.dkr.ecr.eu-central-1.amazonaws.com/demo"
+}
