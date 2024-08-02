f43167383655f292d7e185fddf4a142ef421ea2a
ipluaws <67364414+ipluaws@users.noreply.github.com>
2024-08-02 20:03:26 +0200

    Update main.tf

--- deploy/main.tf
+++ deploy/main.tf
@@ -76,10 +76,4 @@ resource "aws_ecs_service" "app_service" {
     container_name   = "notejam"
     container_port   = 8000
   }
-  lifecycle {
-    ignore_changes = [
-      desired_count,
-      task_definition
-    ]
-  }
 }
